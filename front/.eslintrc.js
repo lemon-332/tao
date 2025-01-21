7425aa56b3f328b5b14d632715e011880df8ce00
lemon254 <783021827@qq.com>
2025-01-21 15:34:10 +0800

    配置修改

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -1,19 +1,24 @@
+// .eslintrc.js
 module.exports = {
-  root: true,
   env: {
-    node: true
+    browser: true,
+    es6: true,
   },
   extends: [
-    'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/typescript/recommended',
-    'plugin:prettier/recommended'
+    'plugin:vue/vue3-recommended', // 如果您使用 Vue 3
+    'prettier' // 确保在最后扩展 Prettier
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2021,
+    sourceType: 'module',
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    'semi': ['error', 'never'], // 与 Prettier 配置一致，不使用分号
+    'quotes': ['error', 'single'], // 使用单引号
+    'indent': ['error', 2, { 'SwitchCase': 1 }], // 使用 2 个空格缩进
+    'comma-dangle': ['error', 'never'], // 不使用尾随逗号
+    'linebreak-style': ['error', 'auto'], // 结尾行格式
+    'vue/html-indent': ['error', 2], // Vue 文件中的缩进
+  },
+};
